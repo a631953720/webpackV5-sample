36afac4f733381520e5e45325e4ab7820bc2929a
kk <a631953720@gmail.com>
2022-07-07 21:54:03 +0800

    feat: remove unused files

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: {
-    index: './src/index.js',
-    print: './src/print.js',
-  },
+  entry: './src/index.js',
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',

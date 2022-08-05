c1d13b6dde9c0111259f62b0adaab8e413d7f8ba
kk <a631953720@gmail.com>
2022-08-06 03:04:22 +0800

    feat: can choose hide row and cells

--- src/constants/env.js
+++ src/constants/env.js
@@ -2,3 +2,5 @@ export const availableTitles = [
   ['編號', '級別', '重量', '顆數', '品名', '單顆重量'],
   ['編號', '售價', '重量', '顆數', '品名', '單顆重量'],
 ];
+
+export const defaultHideTitle = '不刪除';
--- src/index.js
+++ src/index.js
@@ -1,34 +1,96 @@
 import './style.css';
+import $ from 'jquery';
 import downloadPage from './downloadPage.html';
 import saveCSV from './download';
 import showTableAndReturnParseData from './upload';
-import convertCSVData from "./convertCSVData";
+import { convertCSVData, generateListByTitle } from './handleCSVData';
+import { defaultHideTitle } from './constants/env';
 
-// add page into body
-document.body.innerHTML = downloadPage;
-const btn = document.getElementById('save');
+// https://api.jquery.com/ready/#ready-handler-handler
+$(() => {
+  console.log('load!!!');
+  // add page into body
+  document.body.innerHTML = downloadPage;
 
-btn.addEventListener('click', () => {
-  const title = [['編號', '等級', '價格', '重量']];
-  const data = [
-    ['1', 'A', '123456', '66 72 33 56 44 37.2'],
-    ['', '', '', '11 22 33 77 55'],
-    ['2', 'B', 'C11', '11 22 33 77 55'],
-  ];
-  saveCSV({ hideItem: '等級', title, data, hideIndexList: ['1', '3'] });
+  const state = {
+    CSVData: {
+      title: [],
+      items: [],
+    },
+    hideTitle: defaultHideTitle,
+    hideIdList: [],
+    isInit: false,
+  };
+
+  const uploadBtn = document.getElementById('upload');
+
+  uploadBtn.addEventListener('click', async () => {
+    const parse = await showTableAndReturnParseData();
+    const { title, items } = convertCSVData(parse);
+
+    if (title.length < 1 || items.length < 1) {
+      alert('轉換CSV異常');
+      console.log('uploadBtn', { title, items });
+    }
+    state.CSVData = {
+      title,
+      items,
+    };
+
+    console.log(generateListByTitle(title, items, '編號'));
+    const idList = generateListByTitle(title, items, '編號');
+
+    // init hide title list
+    const titleSelector = document.getElementById('titleList');
+    titleSelector.innerHTML = '<option>不刪除</option>';
+    title.forEach((t) => {
+      if (t !== '編號' && t !== '單顆重量') titleSelector.innerHTML += `<option>${t}</option>`;
+    });
+
+    // init hide id list
+    const idSelector = document.getElementById('idList');
+    idSelector.innerHTML = '';
+    idList.forEach((id) => {
+      idSelector.innerHTML += `<option>${id}</option>`;
+    });
+
+    // only init at first time after this condition
+    if (state.isInit) return;
+
+    // init selector event handler
+    $('#titleList').on('change', () => {
+      $('#titleList option:selected').each(function () {
+        // this 為觸發此function的元素
+        state.hideTitle = $(this).text();
+        console.log(state);
+      });
+    });
+
+    $('#idList').on('change', () => {
+      const selects = [];
+      $('#idList option:selected').each(function () {
+        // this 為觸發此function的元素
+        const id = $(this).text();
+        selects.push(id);
+      });
+      state.hideIdList = selects;
+      console.log(state);
+    });
+
+    state.isInit = true;
+  });
+
+  $('#downloadFile').on('click', () => {
+    const { CSVData } = state;
+    const { title, items } = CSVData;
+    if (title.length && items.length) {
+      // 一開始設計的時候，title為一個二維陣列
+      saveCSV({ hideItem: state.hideTitle, hideIndexList: state.hideIdList, title: [title], data: items });
+    }
+  });
 });
 
-
-
-const uploadBtn = document.getElementById('upload');
-uploadBtn.addEventListener('click', async () => {
-  const parse = await showTableAndReturnParseData();
-  const { title, items } = convertCSVData(parse);
-  if (title.length > 0 && items.length > 0) {
-    saveCSV({ hideItem: '級別', title: [title], data: items, hideIndexList: ['1', '2', '3'] });
-  }
-});
-
+// test code
 // const title = [['編號', '等級', '價格', '重量']];
 // const data = [
 //   ['1', 'A', '123456', '66 72 33 56 44 37.2'],

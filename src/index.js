f84aeb22200d22c3d46dd34c2071c4f797dfd865
kk <a631953720@gmail.com>
2022-08-05 23:51:32 +0800

    feat: can read csv file

--- src/index.js
+++ src/index.js
@@ -64,6 +64,7 @@ function saveCSV({ hideItem = '', hideIndexList = [], title = [], data = [] }) {
   link.click();
 }
 
+// add page into body
 document.body.innerHTML = downloadPage;
 const btn = document.getElementById('save');
 
@@ -78,4 +79,57 @@ btn.addEventListener('click', () => {
     ['6', 'V', '876543', '11 22 74'],
   ];
   saveCSV({ hideItem: '等級', title, data, hideIndexList: ['1', '3'] });
+});
+
+function showTableAndReturnParseData() {
+  return new Promise((res, rej) => {
+    const fileUpload = document.getElementById('fileUpload');
+    console.log('is csv', fileUpload.files[0].name.slice(-4) === '.csv');
+    const parse = [];
+    if (fileUpload.files[0].name.slice(-4) === '.csv') {
+      if (typeof FileReader !== 'undefined') {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+          const table = document.createElement('table');
+          const rows = e.target.result.split('\n');
+          for (const element of rows) {
+            const cells = element.split(',');
+            // console.log('cells', cells);
+            parse.push(Array.from(cells));
+
+            // create table
+            if (cells.length > 1) {
+              const row = table.insertRow(-1);
+              for (const element of cells) {
+                const cell = row.insertCell(-1);
+                cell.innerHTML = element;
+              }
+            }
+          }
+          const dvCSV = document.getElementById('dvCSV');
+          dvCSV.innerHTML = '';
+          dvCSV.appendChild(table);
+          res(parse);
+        };
+        reader.readAsText(fileUpload.files[0]);
+      } else {
+        alert('This browser does not support HTML5.');
+        rej('This browser does not support HTML5.');
+      }
+    } else {
+      alert('Please upload a valid CSV file.');
+      rej('Please upload a valid CSV file.');
+    }
+  });
+}
+
+const uploadBtn = document.getElementById('upload');
+uploadBtn.addEventListener('click', async () => {
+  // const file = document.getElementById('uploadFile').files[0];
+  // console.log(file);
+  // console.log(papa.parse(file, {
+  //   download: true
+  // }));
+  const parse = await showTableAndReturnParseData();
+  console.log(parse);
 });

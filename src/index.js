8ab3888326ce59c16408e6724500cb0cf7ef5854
krong <tairong_kang@innodisk.com>
2022-09-19 19:09:35 +0800

    feat: 新增loading範例，刪除多餘檔案

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import 'normalize.css';
 import inputDialog from './component/dialog/example/input';
 import textDialog from './component/dialog/example/text';
 import { showErrorAlert, showSuccessAlert } from './component/alert';
+import { addLoadingElement, removeLoadingElement } from './component/loading';
 
 function init() {
   const root = document.getElementById('root');
+  const box = document.getElementById('box');
   const dialog1 = textDialog();
   const dialog2 = inputDialog();
 
@@ -13,6 +15,9 @@ function init() {
   const inputDialogBtn = document.createElement('button');
   const successAlertBtn = document.createElement('button');
   const errorAlertBtn = document.createElement('button');
+  const addLoadingBtn = document.createElement('button');
+  const removeLoadingBtn = document.createElement('button');
+  const addAllPageLoadingBtn = document.createElement('button');
 
   textDialogBtn.innerHTML = '打開文字Dialog';
   textDialogBtn.addEventListener('click', () => {
@@ -37,6 +42,26 @@ function init() {
     showErrorAlert('456');
   });
   root.appendChild(errorAlertBtn);
+
+  addAllPageLoadingBtn.innerHTML = '打開全頁的loading動畫'
+  addAllPageLoadingBtn.addEventListener('click', () => {
+    const body = document.body;
+    addLoadingElement(body, 'background-color: rgba(0,0,0,0.12);');
+    setTimeout(() => removeLoadingElement(body), 2000);
+  });
+  root.appendChild(addAllPageLoadingBtn);
+
+  addLoadingBtn.innerHTML = '打開loading動畫';
+  addLoadingBtn.addEventListener('click', () => {
+    addLoadingElement(box, 'background-color: rgba(0,0,0,0.12);');
+  });
+  root.before(addLoadingBtn);
+
+  removeLoadingBtn.innerHTML = '關閉loading動畫';
+  removeLoadingBtn.addEventListener('click', () => {
+    removeLoadingElement(box);
+  });
+  root.before(removeLoadingBtn);
 }
 window.onload = () => {
   init();
